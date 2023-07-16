640159d8f8f5b828e55cbb39d94433317be6d29b
Trần Thanh Nhân <66198815+nhanv-dev@users.noreply.github.com>
2023-07-16 20:24:59 +0700

    init

---
+++ contract/assetTransfer.js
@@ -0,0 +1,76 @@
+'use strict';
+import { Contract } from 'fabric-contract-api';
+// import stringify from 'json-stringify-deterministic';
+// import sortKeysRecursive from 'sort-keys-recursive';
+
+class assetTransfer extends Contract {
+
+    async InitLedger(ctx) {
+        const assets = [
+            {
+                ID: 'asset1',
+                Color: 'blue',
+                Size: 5,
+                Owner: 'Tomoko',
+                AppraisedValue: 300,
+            },
+            {
+                ID: 'asset2',
+                Color: 'red',
+                Size: 5,
+                Owner: 'Brad',
+                AppraisedValue: 400,
+            },
+            {
+                ID: 'asset3',
+                Color: 'green',
+                Size: 10,
+                Owner: 'Jin Soo',
+                AppraisedValue: 500,
+            },
+            {
+                ID: 'asset4',
+                Color: 'yellow',
+                Size: 10,
+                Owner: 'Max',
+                AppraisedValue: 600,
+            },
+            {
+                ID: 'asset5',
+                Color: 'black',
+                Size: 15,
+                Owner: 'Adriana',
+                AppraisedValue: 700,
+            },
+            {
+                ID: 'asset6',
+                Color: 'white',
+                Size: 15,
+                Owner: 'Michel',
+                AppraisedValue: 800,
+            },
+        ];
+
+        for (const asset of assets) {
+            asset.docType = 'asset';
+            await ctx.stub.putState(asset.ID, Buffer.from(stringify(sortKeysRecursive(asset))));
+        }
+    }
+
+    async queryMarks(ctx, studentId) {
+
+        let marksAsBytes = await ctx.stub.getState(studentId);
+
+        if (!marksAsBytes || marksAsBytes.toString().length <= 0) {
+
+            throw new Error('Student with this Id does not exist: ');
+
+        }
+
+        let marks = JSON.parse(marksAsBytes.toString());
+        return JSON.stringify(marks);
+
+    }
+}
+
+export default assetTransfer; 
---
+++ contract/index.js
@@ -0,0 +1,5 @@
+import assetTransfer from './assetTransfer';
+
+const AssetTransfer = assetTransfer
+export default AssetTransfer 
+export const contracts = [assetTransfer]; 
